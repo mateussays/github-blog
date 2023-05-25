23b2ab22a7892f3a73a33fea06dd88f6c7dc2f5f
Edrei <edreimateus@gmail.com>
2023-05-25 19:50:47 -0300

    feat:  finalize github profile component

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,8 +1,19 @@
-import { CardContent, Skeleton } from '@mui/material'
-import { ContainerBackground, ProfileCardContainer } from './styles'
+import { CardContent, Skeleton, Typography, Link } from '@mui/material'
+import {
+  AvatarContainer,
+  BioContainer,
+  ContainerBackground,
+  InfoIconsContainer,
+  NameContainer,
+  ProfileCardContainer,
+  ProfileContentContainer,
+  TextInfoIcons,
+  TextInfoIconsContainer,
+} from './styles'
 import { useContext, useEffect, useState } from 'react'
 import { GithubContext } from '../../contexts/GithubContext'
 import theme from '../../styles/themes/default'
+import { Buildings, GithubLogo, Share, Users } from 'phosphor-react'
 
 export function ProfileCard() {
   const { userProfile } = useContext(GithubContext)
@@ -22,13 +33,76 @@ export function ProfileCard() {
             variant="rectangular"
             sx={{
               width: '49rem',
-              height: '3.875rem',
+              height: '7.5rem',
               bgcolor: `${theme['base-post']}`,
               borderRadius: '0.625rem',
             }}
           />
         ) : (
-          <CardContent>{userProfile?.login}</CardContent>
+          <CardContent sx={{ display: 'flex', padding: '0 !important' }}>
+            <AvatarContainer>
+              <img src={userProfile?.avatar_url} alt="Avatar" />
+            </AvatarContainer>
+            <ProfileContentContainer>
+              <NameContainer>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    fontFamily: 'nunito, sans-serif',
+                    color: `${theme['base-title']}`,
+                    fontSize: '1.5rem',
+                    fontWeight: '700',
+                    lineHeight: '2rem',
+                  }}
+                >
+                  {userProfile?.name}
+                </Typography>
+                <Link
+                  href={userProfile?.url}
+                  sx={{
+                    fontFamily: 'nunito, sans-serif',
+                    fontSize: '0.75rem',
+                    fontWeight: '700',
+                    lineHeight: '1.25rem',
+                    color: `${theme['brand-blue']}`,
+                  }}
+                  underline="none"
+                >
+                  GITHUB {''}
+                  <Share size={12} />
+                </Link>
+              </NameContainer>
+              <BioContainer>
+                <Typography
+                  sx={{
+                    fontFamily: 'nunito, sans-serif',
+                    color: `${theme['base-text']}`,
+                    fontSize: '1rem',
+                    fontWeight: '400',
+                    lineHeight: '1.5rem',
+                  }}
+                >
+                  {userProfile?.bio}
+                </Typography>
+              </BioContainer>
+              <InfoIconsContainer>
+                <TextInfoIconsContainer>
+                  <GithubLogo size={18} color={theme['base-label']} />
+                  <TextInfoIcons>{userProfile?.login}</TextInfoIcons>
+                </TextInfoIconsContainer>
+                <TextInfoIconsContainer>
+                  <Buildings size={18} color={theme['base-label']} />
+                  <TextInfoIcons>{userProfile?.company}</TextInfoIcons>
+                </TextInfoIconsContainer>
+                <TextInfoIconsContainer>
+                  <Users size={18} color={theme['base-label']} />
+                  <TextInfoIcons>
+                    {userProfile?.followers} seguidores
+                  </TextInfoIcons>
+                </TextInfoIconsContainer>
+              </InfoIconsContainer>
+            </ProfileContentContainer>
+          </CardContent>
         )}
       </ProfileCardContainer>
     </ContainerBackground>
--- src/components/ProfileCard/styles.ts
+++ src/components/ProfileCard/styles.ts
@@ -1,5 +1,6 @@
-import { Container, Card } from '@mui/material'
+import { Container, Card, Box, Typography } from '@mui/material'
 import styled from 'styled-components'
+import theme from '../../styles/themes/default'
 
 export const ContainerBackground = styled(Container)(() => ({
   justifyContent: 'center',
@@ -19,4 +20,52 @@ export const ProfileCardContainer = styled(Card)(({ theme }) => ({
     boxShadow: '0px 2px 28px rgba(0, 0, 0, 0.2) ',
     borderRadius: '0.625rem ',
   },
+}))
+
+export const AvatarContainer = styled(Box)(() => ({
+  width: '9.25rem',
+  maxWidth: '100%',
+
+  img: {
+    width: '9.25rem',
+    maxWidth: '100%',
+    height: 'auto',
+    borderRadius: '0.5rem',
+  },
+}))
+
+export const ProfileContentContainer = styled(Box)(() => ({
+  marginLeft: '2rem',
+  width: '38.25rem',
+  maxWidth: '100%',
+}))
+
+export const NameContainer = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}))
+
+export const BioContainer = styled(Box)(() => ({
+  marginTop: '0.5rem',
+}))
+
+export const InfoIconsContainer = styled(Box)(() => ({
+  display: 'flex',
+  marginTop: '1.5rem',
+}))
+
+export const TextInfoIconsContainer = styled(Box)(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  color: `${theme['base-subtitle']}`,
+  marginRight: '1.5rem',
+}))
+
+export const TextInfoIcons = styled(Typography)(() => ({
+  fontFamily: 'nunito !important',
+  fontSize: '1rem',
+  fontWeight: '400',
+  lineHeight: '1.5rem',
+  padding: '0.5rem',
 }))
